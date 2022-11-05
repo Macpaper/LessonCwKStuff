71a253ed2c2cce38317396fd24cde1a7e9c23bbf
Mack Benham <macpaper23@yahoo.com>
2022-11-05 13:21:42 -0400

    balh

---
+++ main/Game.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Game {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("completely new game");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new GamePanel());
+		f.pack();
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+	}
+
+}
+
+
+// WARMUP: Add an enemy class to the game. Give them an x and y velocity,
+// then make it so they collide when they hit the edges of the window.
---
+++ module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author mackb
+ *
+ */
+module Lesson4 {
+	requires java.desktop;
+}
